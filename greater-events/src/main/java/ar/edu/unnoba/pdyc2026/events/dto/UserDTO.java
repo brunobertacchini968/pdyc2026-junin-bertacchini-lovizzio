ff26b72169119e210d67ac4492970ec18832b3fa
LucasLovizzio <llovizzio@comunidad.unnoba.edu.ar>
2026-06-08 22:41:17 -0300

    Add User entity, repository and basic DTOs

---
+++ greater-events/src/main/java/ar/edu/unnoba/pdyc2026/events/dto/UserDTO.java
@@ -0,0 +1,47 @@
+package ar.edu.unnoba.pdyc2026.events.dto;
+
+import ar.edu.unnoba.pdyc2026.events.model.User;
+
+public class UserDTO {
+
+    private Long id;
+    private String username;
+    private String email;
+
+    public UserDTO() {
+    }
+
+    public UserDTO(Long id, String username, String email) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+    }
+
+    public static UserDTO fromEntity(User user) {
+        return new UserDTO(user.getId(), user.getUsername(), user.getEmail());
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
---
+++ greater-events/src/main/java/ar/edu/unnoba/pdyc2026/events/model/User.java
@@ -0,0 +1,93 @@
+package ar.edu.unnoba.pdyc2026.events.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @ManyToMany
+    @JoinTable(
+            name = "user_followed_artists",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "artist_id")
+    )
+    private Set<Artist> followedArtists = new HashSet<>();
+
+    @ManyToMany
+    @JoinTable(
+            name = "user_favorite_events",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "event_id")
+    )
+    private Set<Event> favoriteEvents = new HashSet<>();
+
+    public User() {
+    }
+
+    public User(String username, String email) {
+        this.username = username;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Set<Artist> getFollowedArtists() {
+        return followedArtists;
+    }
+
+    public void setFollowedArtists(Set<Artist> followedArtists) {
+        this.followedArtists = followedArtists;
+    }
+
+    public Set<Event> getFavoriteEvents() {
+        return favoriteEvents;
+    }
+
+    public void setFavoriteEvents(Set<Event> favoriteEvents) {
+        this.favoriteEvents = favoriteEvents;
+    }
+}
