62e5704a979800c49523d4d407ef9ff72519462c
LucasLovizzio <llovizzio@comunidad.unnoba.edu.ar>
2026-06-08 22:42:43 -0300

    Add Keycloak authorities converter

---
+++ greater-events/src/main/java/ar/edu/unnoba/pdyc2026/events/security/KeycloakJwtGrantedAuthoritiesConverter.java
@@ -0,0 +1,58 @@
+package ar.edu.unnoba.pdyc2026.events.security;
+
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.jwt.Jwt;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class KeycloakJwtGrantedAuthoritiesConverter implements Converter<Jwt, Collection<GrantedAuthority>> {
+
+    @Override
+    public Collection<GrantedAuthority> convert(Jwt jwt) {
+        Set<GrantedAuthority> authorities = new HashSet<>();
+        authorities.addAll(extractRoles(jwt.getClaim("realm_access")));
+        authorities.addAll(extractResourceRoles(jwt.getClaim("resource_access")));
+        return authorities;
+    }
+
+    private Collection<GrantedAuthority> extractRoles(Object claim) {
+        if (!(claim instanceof Map<?, ?> claimMap)) {
+            return List.of();
+        }
+
+        Object roles = claimMap.get("roles");
+        if (!(roles instanceof Iterable<?> iterableRoles)) {
+            return List.of();
+        }
+
+        List<GrantedAuthority> authorities = new ArrayList<>();
+        for (Object role : iterableRoles) {
+            if (role instanceof String roleName && !roleName.isBlank()) {
+                authorities.add(new SimpleGrantedAuthority("ROLE_" + roleName));
+            }
+        }
+        return authorities;
+    }
+
+    private Collection<GrantedAuthority> extractResourceRoles(Object claim) {
+        if (!(claim instanceof Map<?, ?> claimMap)) {
+            return List.of();
+        }
+
+        Set<GrantedAuthority> authorities = new HashSet<>();
+        for (Object clientClaim : claimMap.values()) {
+            if (!(clientClaim instanceof Map<?, ?> clientMap)) {
+                continue;
+            }
+            authorities.addAll(extractRoles(clientMap));
+        }
+        return authorities;
+    }
+}
